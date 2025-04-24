cf76a23a0424aae67977f27e7c5a18b4870f3111
Mohammad Naserian <nobleyam0@gmail.com>
2025-04-24 21:48:00 +0300

    add random number in sudoku

--- MD_naserian/week 6/sudoku.py
+++ MD_naserian/week 6/sudoku.py
@@ -4,7 +4,9 @@
 # 3.other lines: 1
 # 4.hide turtle
 # 5.highest speed
+# 6.fill sudoku
 import turtle
+import random
 
 
 drawer = turtle.Turtle()
@@ -28,10 +30,10 @@ angle = 90
 for i in range(1, 9):
     if i % 3==0:
         drawer.pensize(3)
-        drawer.forward(out/8)
+        drawer.forward(out/9)
     else:
         drawer.pensize(1)
-        drawer.forward(out/8)
+        drawer.forward(out/9)
 
     drawer.right(angle)
     drawer.forward(out)
@@ -40,17 +42,20 @@ for i in range(1, 9):
     angle *=-1
 
 
+
+
 # 2. horizental lines 3*3 lines : 3
+drawer.forward(out/9)
 drawer.setheading(-90)
 
 
-for i in range(1, 9):
+for i in range(1,9):
     if i % 3==0:
         drawer.pensize(3)
-        drawer.forward(out/8)
+        drawer.forward(out/9)
     else:
         drawer.pensize(1)
-        drawer.forward(out/8)
+        drawer.forward(out/9)
 
     drawer.right(angle)
     drawer.forward(out)
@@ -58,14 +63,38 @@ for i in range(1, 9):
     angle *=-1
 
 
+# inserting the numbers:
+
+y = 470
+for j in range(9):
+    x_base  = -400
+    y -=100
+    numbers = random.sample(range(1, 10), k=5)
+    filled_positions = random.sample(range(9), k=5)
+    for pos, num in zip(filled_positions, numbers):
+        turtle.penup()
+        turtle.hideturtle()
+        x = x_base + pos * 100
+        turtle.goto(x, y)
+        turtle.speed(0)
+        turtle.write(num,  font=("courier", 24, "bold"))
+        
+    
 
 
 
+# try to find X and Y
+def btnclick(x, y):
+    turtle.clear()
+    turtle.hideturtle()
+    text = f"x = {x}, y = {y}\n"
+    turtle.write(text, align="center", font=("courier", 18, "normal"))
+    with open("MD_naserian\\week 6\\location.txt", "a") as f:
+        f.write(text)
+    
 
 
 
-
-
-
-
-turtle.done()
+turtle.onscreenclick(btnclick, 1)
+turtle.listen()
+turtle.mainloop()
